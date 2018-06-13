b8448bc6a7a9a256fe16ec210c8143aac6057be2
Christopher Shaw <chris@chris-shaw.com>
2018-06-13 10:32:49 +0100

    Create DataCache.php

---
+++ DataCache.php
@@ -0,0 +1,22 @@
+<?php
+
+use Exception;
+class DataCache {
+	public function __construct ($file, $timeout=180) {
+		$this->file = "/tmp/DataCache-{$file}.dat";
+		$this->cache_time = time() - $timeout;
+	}
+	public function read () {
+		$content = file_get_contents($this->file);
+		$modified = filemtime($this->file);
+		if ($modified < $this->cache_time) {
+			unlink($this->file);
+			return false;
+			
+		}
+		return $content;
+	}
+	public function write ($data=false) {
+		return file_put_contents($this->file, $data);
+	}
+}
